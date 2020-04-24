c88be2cf1e04716535ee9e666fa50df9a27da26a
sheng <shengzidong@126.com>
2020-04-24 15:29:56 +0800

    upgrade to AndroidX

--- messagebubble/src/main/java/com/szd/messagebubble/MessageBubbleView.java
+++ messagebubble/src/main/java/com/szd/messagebubble/MessageBubbleView.java
@@ -15,7 +15,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
